a86838a299facf7daac46407f0601777c04a1c74
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2025-10-26 09:47:29 -0300

    Update FireworkRocketEntityMixin.java

--- src/main/java/me/duncanruns/dragqueen/mixin/FireworkRocketEntityMixin.java
+++ src/main/java/me/duncanruns/dragqueen/mixin/FireworkRocketEntityMixin.java
@@ -7,17 +7,13 @@ import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(FireworkRocketEntity.class)
 public abstract class FireworkRocketEntityMixin {
-    @Shadow
-    private LivingEntity shooter;
-
     @WrapOperation(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
     private void addNotSet(LivingEntity guy, Vec3d vec3d, Operation<Void> original) {
         double speed = MathHelper.sqrt(Math.max(vec3d.lengthSquared(), guy.getVelocity().lengthSquared()));
-        original.call(guy, vec3d.normalize().multiply(speed + 0.1D));
+        original.call(guy, guy.getRotationVector().normalize().multiply(speed + 0.1D));
     }
 }
